fec2c78a2d9fb3c6f6cd8c0510637feea5052387
Claudiney clayton <35464754+clarepe@users.noreply.github.com>
2024-03-18 22:23:43 +0000

    desafio2

--- desafio3.js
+++ desafio3.js
@@ -1,5 +1,11 @@
 const temperaturaAtual = window.prompt ('Digite a temperatura atual entre Fahrenheit, Celsius ou Kelvin:');
 const escalaAtual = window.prompt ('Escolha a escala atual Fahrenheit, Celsius ou Kelvin');
 const conversao =window.prompt ('Escolha a escala a ser convertida Fahrenheit, Celsius ou Kelvin');
-let calculo=0;
+let resultado=0;
 
+if(escalaAtual.toLowerCase() === 'celsius'){
+    if(conversao.toLowerCase() === 'fahrenheit'){
+        
+    }
+    
+}
