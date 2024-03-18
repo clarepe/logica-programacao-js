a6c45e260170e28fc4645ade45339f05735d25de
Claudiney clayton <35464754+clarepe@users.noreply.github.com>
2024-03-18 17:51:17 +0000

    fazendo desafio3

---
+++ desafio3.js
@@ -0,0 +1,5 @@
+const temperaturaAtual = window.prompt ('Digite a temperatura atual entre Fahrenheit, Celsius ou Kelvin:');
+const escalaAtual = window.prompt ('Escolha a escala atual Fahrenheit, Celsius ou Kelvin');
+const conversao =window.prompt ('Escolha a escala a ser convertida Fahrenheit, Celsius ou Kelvin');
+let calculo=0;
+
