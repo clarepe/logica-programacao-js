3e5ba87d260b2ce4245f24b1e232daf402b84a6c
Claudiney clayton <35464754+clarepe@users.noreply.github.com>
2024-03-19 14:25:52 +0000

    terminei d3

--- desafio3.js
+++ desafio3.js
@@ -1,11 +1,30 @@
-const temperaturaAtual = window.prompt ('Digite a temperatura atual entre Fahrenheit, Celsius ou Kelvin:');
-const escalaAtual = window.prompt ('Escolha a escala atual Fahrenheit, Celsius ou Kelvin');
-const conversao =window.prompt ('Escolha a escala a ser convertida Fahrenheit, Celsius ou Kelvin');
-let resultado=0;
+const
+    temperaturaAtual = parseFloat(window.prompt('Digite a temperatura atual entre Fahrenheit, Celsius ou Kelvin:')),
+    escalaAtual = window.prompt('Escolha a escala atual Fahrenheit, Celsius ou Kelvin').toLowerCase(),
+    conversao = window.prompt('Escolha a escala a ser convertida Fahrenheit, Celsius ou Kelvin').toLowerCase();
+ 
+const
+    CelFahr = (temperaturaAtual * 9/5) + 32,
+    CelKev = temperaturaAtual + 273.15,
+    FahrCel = (temperaturaAtual-32)* 5/9,
+    FahrKel = (temperaturaAtual + 459.67) * 5/9,
+    KelCel = (temperaturaAtual) -273.15,
+    KelFahr = temperaturaAtual * 9/5 -459.67;
 
-if(escalaAtual.toLowerCase() === 'celsius'){
-    if(conversao.toLowerCase() === 'fahrenheit'){
-        
-    }
-    
+if  (escalaAtual ==='c' && conversao ==='f'){
+    window.alert(`A temperatura atual em Fahrenheit é ${CelFahr.toFixed(2)}`);
+} else if (escalaAtual==='c' && conversao === 'k' ){
+    window.alert(`A tempetura atual em Kelvin é ${CelKev.toFixed(2)}`);
+}else if(escalaAtual==='f' && conversao==='c'){
+    window.alert(`A temperatura atual em Celsius é ${FahrCel.toFixed(2)}`);
+}else if (escalaAtual==='f'&& conversao ==='k'){
+    window.alert(`A temperatura atual em Kevin é ${FahrKel.toFixed(2)}`);
+}else if (escalaAtual ==='k'&& conversao==='c'){
+    window.alert(`A temperatura atual em  Celsius é ${KelCel.toFixed(2)}`);
+}else if (escalaAtual==='k'&& conversao==='f'){
+    window.alert(`A temperatura atual em Fahrenheit é ${KelFahr.toFixed(2)}`);
 }
+else{
+    window.alert('Impossivel reconhecer os dados, verifique se digitou tudo corretamente')
+}
+
