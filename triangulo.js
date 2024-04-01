b578612bffdfd6d393f457cbc67fe8f5c375ec7a
clarepe <claudineyclayton@yahoo.com.br>
2024-04-01 11:45:35 -0300

    adicionando novos apps

---
+++ triangulo.js
@@ -0,0 +1,26 @@
+
+
+function calcula_lados(ladoA,ladoB,ladoC){
+
+    let resultado;
+
+    if(ladoA===ladoB && ladoB===ladoC && ladoA===ladoC){
+       
+           resultado= `${ladoA,ladoB,ladoC}Equilátero: todos os lados tem o mesmo tamanho`;
+        }
+     
+    
+    else if( ladoA=!ladoB){
+        
+            resultado=`${ladoA,ladoB,ladoC} Isósceles: dois lados tem o mesmo tamanho, mas o terceiro é diferente`;
+                      
+         }        
+    
+    else if(ladoA!=ladoB){
+        
+            resultado=`${ladoA,ladoB,ladoC} Escaleno: os três lados do triângulo tem tamanhos diferentes`;
+        }
+      
+    }
+     return resultado;
+}
