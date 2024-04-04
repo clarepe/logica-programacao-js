7ef8be276ea4e407f14d287137f3e94465fd5320
Claudiney clayton <claudineyclayton@yahoo.com.br>
2024-04-04 19:04:28 -0300

    pronto o jogo

--- futebol.js
+++ futebol.js
@@ -1,20 +1,14 @@
-//3. Jogo de Futebol
-//Desenvolva um programa que receba do usuário o placar de um jogo de futebol (os gols de cada time), e
-//  informa se o resultado foi empate, vitória do primeiro time ou vitória do segundo time.
+// futebol.js
+function jogoFutebol(local, visitante) {
+    let resultado;
 
-function jogoFutebol(timeLocal,timeVisitante){
-
-    let local,visitante,resultado;
-
-    function resultado(){
-        if(local===visitante){
-            resultado="empate";
-        }
-        else if(local>visitante){
-            resultado="vitória do primeiro time";
-        }
-        else{resultado="vitória do segundo time";}
-        return resultado;
+    if (local === visitante) {
+        resultado = "empate";
+    } else if (local > visitante) {
+        resultado = "vitória do primeiro time";
+    } else {
+        resultado = "vitória do segundo time";
     }
 
+    return resultado;
 }
